c6636249f9d78e9b37cc73bc3b803edf12498f2a
Thomas Martineau <mart0657@gmail.com>
2019-05-13 15:13:38 -0400

    Finished the lesson

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: const Color(0xFF6C8BA0),
+        backgroundColor: Color(0xFF6C8BA0),
         body: SafeArea(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -32,8 +32,55 @@ class MyApp extends StatelessWidget {
                 style: TextStyle(
                   fontFamily: 'Source Sans Pro',
                   fontSize: 24.0,
-                  color: const Color(0xFFF99D1C),
+                  color: Color.fromRGBO(255, 255, 255, 0.75),
                   letterSpacing: 2,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 200.0,
+                child: Divider(
+                  color: Color(0xFFF99D1C),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Color(0xFF6C8BA0),
+                  ),
+                  title: Text(
+                    '613-325-4589',
+                    style: TextStyle(
+                      color: Color(0xFF6C8BA0),
+                      fontSize: 20.0,
+                      fontFamily: 'Source Sans Pro',
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Color(0xFF6C8BA0),
+                  ),
+                  title: Text(
+                    'thomas@mydomastudio.com',
+                    style: TextStyle(
+                      color: Color(0xFF6C8BA0),
+                      fontSize: 20.0,
+                      fontFamily: 'Source Sans Pro',
+                    ),
+                  ),
                 ),
               ),
             ],
